77251be91aa9935d445a62c4936fad72b0434b51
Sheikh MiZan <sheikhmizanbd@gmail.com>
2023-07-07 14:03:35 +0600

    Create insertion_js.js

---
+++ Insertion Sort/insertion_js.js
@@ -0,0 +1,12 @@
+function insertionSort(arr) {
+    var n = arr.length;
+    for (var i = 1; i < n; i++) {
+        var key = arr[i];
+        var j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
