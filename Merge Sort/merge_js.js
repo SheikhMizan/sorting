e8b63c73f50942b378a48dd6b85ea95f299e81f9
Sheikh MiZan <sheikhmizanbd@gmail.com>
2023-07-07 14:05:14 +0600

    Create merge_js.js

---
+++ Merge Sort/merge_js.js
@@ -0,0 +1,15 @@
+function merge(left, right) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+    return result.concat(left.slice(i)).concat(right.slice(j));
+}
